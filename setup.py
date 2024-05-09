1b82e6240bb1e26219fa270bdaf2ed28877d4b77
hiro <80311024+hiro-cryhack@users.noreply.github.com>
2024-05-09 20:29:08 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simpleblockchain',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # 列出你的项目依赖的所有包
+    ],
+    # 添加其他元数据，如作者、项目描述等
+)
